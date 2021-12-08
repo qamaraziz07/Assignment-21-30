8948ae7751864032340ddf4d6d400d874d4b3695
Qamar Aziz <59906689+qamaraziz07@users.noreply.github.com>
2021-12-08 20:17:01 +0500

    Add files via upload

---
+++ Resolve Asdignment 21-30/roundingNumber.js
@@ -0,0 +1,33 @@
+//<<Q:1>>
+// var user = prompt("Enter a number..!!");
+// var number =Number(user);
+// var rondOf =Math.round(number);
+// var flor =Math.floor(number);
+// var cel =Math.ceil(number);
+// document.write("Number : "+ number +"<br>"+"floor Value : "+ flor+"<br>"+"Round Of Value :"+rondOf+"<br>"+"Ceil Value : "+cel);
+
+//<<Q:2>>
+//  var user = prompt("Enter a number..!!");
+// var number =Number(user);
+// var rondOf =Math.round(number);
+// var flor =Math.floor(number);
+// var cel =Math.ceil(number);
+// document.write("Number : "+ number +"<br>"+"floor Value : "+ flor+"<br>"+"Round Of Value :"+rondOf+"<br>"+"Ceil Value : "+cel);
+
+//<<Q:3>>
+// var user =+prompt("enter value");
+// var num =Math.abs(user);
+// document.write("The absolute Value of "+user+" is "+num);
+
+//<<Q:4>>
+// var dice=Math.random();
+// var num =(dice * 6);
+// var newNum =Math.ceil(num);
+// document.write("Random Dice Value : "+newNum);
+
+
+///<<Q:6>>
+// var rndNum=Math.random();
+// var num =(rndNum * 100) + 1;
+// var newNum =Math.floor(num);
+// document.write("Random number between 1 to 100 : "+newNum);
---
+++ Resolve Asdignment 21-30/string.js
@@ -0,0 +1,96 @@
+//<<Q:1>>
+// var user1= prompt("Enter Your First Name..!!");
+// var user2= prompt("Enter Your Second Name..!!");
+// var fullName= user1 + user2;
+// alert("Dear user Your Full Name is..\n"+fullName);
+
+///<<Q:2>>
+// var user =prompt("Enter your Favourite Mobile Phone Model..!!");
+// document.write("My Favourite Phone is: "+ user +"<br>"+"Length of a String is : "+user.length);
+
+///<<Q:3>>
+// var word ="Pakistani";
+// var fIndx = word.indexOf('n');
+// document.write("String : " +word+"<br>"+"index of 'n' : " +fIndx);
+
+//<<Q:4>>
+// var word ="Hello World";
+// var fIndx = word.lastIndexOf("l");
+// document.write("String : " +word+"<br>"+" Last index of 'l' : " +fIndx);
+
+///<<Q:5>>
+// var word ="Pakistani";
+// var fIndx = word.charAt(3);
+// document.write("String : " +word+"<br>"+" Character at index 3: " +fIndx);
+
+///<<Q:6>>
+// var user1= prompt("Enter Your First Name..!!");
+// var user2= prompt("Enter Your Second Name..!!");
+// var fullName= user1.concat(user2);
+// alert("Dear user Your Full Name is..\n"+fullName);
+
+//<<Q:7>>
+// var word = " Hyderabad ";
+// var updatedName = word.replace('Hyder','Islam');
+// document.write("City :"+word+"<br>"+"After replacement : " +updated Name);
+
+//<<Q:8>>
+// var message ="Ali and Sami are best friends. They play cricket and football together.";
+// var newMsg = message.replace(/and/g,'&');
+// document.write("Current Message :"+message+"<br>" +"Updated Message : "+newMsg);
+
+//<<Q:9>>
+// var num ='472';
+// var updNum = parseInt(num);
+// document.write("Value :"+num+"<br>"+"Type : "+typeof(num)+"<br>"+"Value : "+updNum +"<br>"+"Type : "+typeof(updNum));
+
+//<<Q:10>>
+// var user= prompt("Enter a Any Word ..!!");
+// var updWord =user.toUpperCase();
+// document.write("User input : "+user+"<br>"+"Upper Case : "+updWord);
+
+///<<Q:11>>
+// var user= prompt("Enter a Any Word ..!!");
+// updWord=user[0].toUpperCase() + 
+//             user.slice(1)
+// document.write("User input : "+user+"<br>" +
+// "Title Case : "+updWord);
+
+///<<Q:12>>
+// var num=35.36;
+// var numIntoStr = num.toString();
+// var user=numIntoStr.replace('.',"")
+// document.write("Number : "+ num +"<br>"+"Result : "+user);
+
+///<<Q:17>>
+// var word =prompt("Enter  any Word..!!");
+// var fIndx = word.charAt(word.length-1);
+// document.write("User Input : " +word+"<br>"+"Last Character of Input : " +fIndx);
+
+///<<Q:14>>
+
+// var items=["cake","apple pie","chips","cookie","patties"];
+// var itemToCheck =prompt("Welcome to ABC Bakery.What do you want to order Sir/Mam?");
+// var check = false
+// for(var i=0; i<=items.length; i++){
+// if(itemToCheck === items[i]){
+//     document.write(items[i]+ " is available at index "+ i + " in our Bakery");
+//     var check = true
+// }
+// };
+// if(!check){
+//     document.write("we are sorry that was not available in the baker....");
+// }
+
+///<<Q:13>>
+// Not understand the logic that is used in it..
+
+
+//<<Q:16>>
+// var university = "University of Karachi";
+//         var splt=  university.split(" ");
+//            var a =[...university];
+//            for( var i=0; i<a.length; i++){
+//                 document.write(a[i]+"<br>");
+//            };
+
